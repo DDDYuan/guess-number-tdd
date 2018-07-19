abfd4bc21bc4c15bf332777c55c97b71b9a1c488
Di Yuan <dyuan@thoughtworks.com>
2018-07-19 17:17:13 +0800

    extend answer generator

--- answer-generator.js
+++ answer-generator.js
@@ -1,9 +1,13 @@
 class AnswerGenerator {
-    constructor() {}
+    constructor(min, max, length) {
+        this.min = min;
+        this.max = max;
+        this.length = length;
+    }
     generate() {
         const answer = [];
-        while (answer.length < 4) {
-            const number = ~~(Math.random() * 10);
+        while (answer.length < this.length) {
+            const number = ~~(Math.random() * (this.max - this.min + 1)) + this.min;
             if (!answer.includes(number)) {
                 answer.push(number);
             }
