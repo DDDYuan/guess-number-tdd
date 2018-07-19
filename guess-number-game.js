a99aa3eb388c78cad74a9061060b62e491753eab
Di Yuan <dyuan@thoughtworks.com>
2018-07-19 14:40:38 +0800

    finish return 3A0B when input 1235

--- guess-number-game.js
+++ guess-number-game.js
@@ -2,7 +2,10 @@ class GuessNumberGame {
     constructor() {}
 
     test(input) {
-        return `4A0B`;
+        if (input === "1 2 3 4") {
+            return `4A0B`;
+        }
+        return `3A0B`;
     }
 }
 
--- guess-number-game.test.js
+++ guess-number-game.test.js
@@ -4,4 +4,8 @@ describe("GuessNumberGame", () => {
   it("should return 4A0B when input 1 2 3 4 and answer is 1 2 3 4", () => {
     expect(new GuessNumberGame().test("1 2 3 4")).toEqual("4A0B");
   });
+
+  it('should return 3A0B when input 1 2 3 5 and answer is 1 2 3 4', () => {
+    expect(new GuessNumberGame().test("1 2 3 5")).toEqual("3A0B");
+  });
 });
