28e28fbe017b2f8271866e4b3892d0a80de1336f
Di Yuan <dyuan@thoughtworks.com>
2018-07-19 15:01:17 +0800

    finish return 0A3B when input 0 1 2 3

--- guess-number-game.js
+++ guess-number-game.js
@@ -5,12 +5,15 @@ class GuessNumberGame {
 
     test(input) {
         let numberInCorrectPosition = 0;
+        let numberInWrongPosition = 0;
         for (let index in input.split(" ")) {
             if (input.split(" ")[index] === this.answer.split(" ")[index]) {
                 numberInCorrectPosition += 1;
+            } else if (this.answer.split(" ").includes(input.split(" ")[index])) {
+                numberInWrongPosition += 1;
             }
         }
-        return `${numberInCorrectPosition}A0B`;
+        return `${numberInCorrectPosition}A${numberInWrongPosition}B`;
     }
 }
 
--- guess-number-game.test.js
+++ guess-number-game.test.js
@@ -12,4 +12,8 @@ describe("GuessNumberGame", () => {
   it('should return 2A0B when input 1 0 3 5 and answer is 1 2 3 4', () => {
     expect(new GuessNumberGame().test("1 0 3 5")).toEqual("2A0B");
   });
+
+  it('should return 0A3B when input 0 1 2 3 and answer is 1 2 3 4', () => {
+    expect(new GuessNumberGame().test("0 1 2 3")).toEqual("0A3B");
+  });
 });
